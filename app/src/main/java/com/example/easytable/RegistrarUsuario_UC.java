28f83a1b22b9ee8e3347f7a0409ea29d13f95859
Alvaro Corona <alvariux2008@gmail.com>
2021-02-08 18:02:47 -0600

    Se agrego la primer vista de las vistas RegistrarUsuario_UC

---
+++ app/src/main/java/com/example/easytable/RegistrarUsuario_UC.java
@@ -0,0 +1,14 @@
+package com.example.easytable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarUsuario_UC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario__u_c);
+    }
+}
