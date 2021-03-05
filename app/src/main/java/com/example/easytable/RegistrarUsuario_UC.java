940324d9a3103afdf01617486fec4f57cd7658a2
Alvaro Corona <alvariux2008@gmail.com>
2021-03-05 03:23:01 -0600

    Se realizo la vista de RegistrarUsuario_UC y se añadio el spinner para ingresar si es cliente o dueño de local

--- app/src/main/java/com/example/easytable/RegistrarUsuario_UC.java
+++ app/src/main/java/com/example/easytable/RegistrarUsuario_UC.java
@@ -4,13 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Toast;
 
 public class RegistrarUsuario_UC extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_registrar_usuario__u_c);
-    }
+
 }
