4d786ef79c33888a8136715eb4c33228a51b3dae
Fernando Adame <adamef640@gmail.com>
2021-07-30 02:18:03 -0500

    ventas del DL

--- app/src/main/java/com/example/easytable/Ventas.java
+++ app/src/main/java/com/example/easytable/Ventas.java
@@ -1,39 +1,73 @@
 package com.example.easytable;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Ventas extends AppCompatActivity {
 
     private TextView mTotalVentas;
     private FirebaseFirestore db;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vista_ventas);
 
         mTotalVentas = findViewById(R.id.totalVentas);
         db = FirebaseFirestore.getInstance();
 
+
         Bundle extra = getIntent().getExtras();
         String idRestaurante = extra.getString("idRestaurante");
 
-        DocumentReference doc = db.collection("restaurante").document(idRestaurante);
-        doc.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+        Query doc = db.collection("cuenta").whereEqualTo("idDelLocal", idRestaurante);
+        doc.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                String ventas = value.get("ventas").toString();
-                mTotalVentas.setText(ventas);
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                        DocumentReference docRef = db.collection("cuenta").document(document.getId());
+                        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists())
+                                        Toast.makeText(Ventas.this, document.getId()+ "    :->           "
+                                                +document.get("montoPagar").toString(), Toast.LENGTH_SHORT).show();
+                                    else {
+                                    }
+
+                                } else {
+                                }
+                            }
+                        });
+                    }
+                }
             }
         });
     }
